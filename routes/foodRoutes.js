a271c75ac8978a8180c52bc70aee5d47532bf0b2
SpiritCorder <60997354+SpiritCorder@users.noreply.github.com>
2023-05-06 22:24:58 +0530

    final finished backend

---
+++ routes/foodRoutes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+
+const {auth, isEmployee} = require('../middleware/auth');
+
+router.use(auth);
+
+const {createNewMenu, getAllMenus, deleteMenu, getSingleMenu, updateSingleMeal, getSingleMenuUser, createNewOrder, getPopularCategories} = require('../controllers/foodControllers');
+
+router.route('/')
+    .get(getAllMenus)
+    .post(isEmployee, createNewMenu)
+
+router.route('/order')
+    .post(createNewOrder) // place a new food order
+
+router.route('/popular-categories')
+    .get(isEmployee, getPopularCategories)
+
+router.route('/user/:menuId')
+    .get(getSingleMenuUser)
+
+router.route('/:menuId')
+    .get(getSingleMenu)
+    .delete(isEmployee, deleteMenu)
+
+
+
+router.route('/meal/update')
+    .put(isEmployee, updateSingleMeal)
+
+
+module.exports = router;
